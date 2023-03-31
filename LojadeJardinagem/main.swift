3be97fc581efe2f2e4623975d00363d60abf6315
Felipe Guimaraes <lipefrances@yahoo.com.br>
2023-03-31 14:42:56 -0300

    adicionei função  addcarrinho. Inclui códigos das opções 3 e 4 do menu principal

--- LojadeJardinagem/main.swift
+++ LojadeJardinagem/main.swift
@@ -16,6 +16,7 @@ dict_prod[4] = ["Cresce rápido", "342", "4.80", "fertilizante"]
 dict_prod[5] = ["Vai", "342", "4.80", "fertilizante"]
 dict_prod[6] = ["Cresce muito muito rápido", "342", "4.80", "fertilizante"]
 
+var carrinho = [Int: Int]()
 var controle = "2"
 
 repeat {
@@ -23,35 +24,64 @@ repeat {
     print("Bem-vindo a loja Adube-se. Aqui temos produtos para todos os tipos de plantas.")
     print("1. Listar todos os produtos")
     print("2. Buscar produto por categoria")
-    print("3. Ver carrinho")
+    print("3. Adicionar produto ao carrinho")
+    print("4. Ver carrinho")
     print("9. Encerrar")
 
     print("")
     print("O que voce deseja fazer? Digite o numero correspondente")
 
-    let input = readLine()
+    let input = readLine() ?? ""
     
+
     print("")
     
     // Encerrar sistema
     switch input{
     case "1":
         listarProdutos()
+    
     case "2":
         print("2")
+    
+    case "3":
+        addcarrinho()
+    
+    case "4":
+        if carrinho.isEmpty {
+            print("Carrinho vazio.")
+        } else {
+            print("Itens no carrinho:")
+            print("---------------------------------------------------------------")
+            print("|             Nome             |            Preço             | Qtd |")
+            print("---------------------------------------------------------------")
+            for (id, qtd) in carrinho {
+                let prod = dict_prod[id]!
+                let nome = prod[0]
+                let preco = prod[2]
+                print("|", nome.padding(toLength: 31, withPad: " ", startingAt: 0), "|", preco.padding(toLength: 30, withPad: " ", startingAt: 0), "| \(qtd) |")
+                print("---------------------------------------------------------------")
+            }
+        }
+
+        
+
     case "9":
         controle = "1"
     default:
         print("Opcao invalida")
     }
 
-    if controle != "1" {
+    if controle != "1" {                            //nao esta funcionando. o loop reinicia independente do numero escolhido, 9 encerra.
         print("")
         print("0. Voltar ao menu anterior")
+        print("3. Adicionar produtos ao carrinho")
         print("9. Encerrar programa")
         
         let input2 = readLine()
-        
+        if(input2 == "3") {
+           addcarrinho()
+        }
         if (input2 == "9") {
             controle = "1"
         }
@@ -86,3 +116,30 @@ func listarProdutos (){
     }
 }
 
+func addcarrinho(){
+    print("Adicionar produto ao carrinho")
+    print("Digite o ID do produto:")
+    if let id = Int(readLine() ?? "") {
+        if dict_prod[id] != nil {
+            print("Digite a quantidade:")
+            if let qtd = Int(readLine() ?? "") {
+                if qtd > 0 {
+                    if carrinho[id] != nil {
+                        carrinho[id]! += qtd
+                    } else {
+                        carrinho[id] = qtd
+                    }
+                    print("\(qtd) unidades do produto \(dict_prod[id]![0]) adicionadas ao carrinho.")
+                } else {
+                    print("Quantidade inválida. Tente novamente.")
+                }
+            } else {
+                print("Quantidade inválida. Tente novamente.")
+            }
+        } else {
+            print("Produto não encontrado. Tente novamente.")
+        }
+    } else {
+        print("ID inválido. Tente novamente.")
+    }
+}
